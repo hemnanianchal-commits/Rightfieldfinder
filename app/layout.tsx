0a44fde6f0bb936f4902b777ab26eb88409505b2
hemnanianchal-commits <hemnanianchal@gmail.com>
2026-09-17 22:43:28 +0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,6 +23,9 @@ export const viewport: Viewport = {
 
 export const metadata: Metadata = {
   metadataBase: new URL('https://rightfieldfinder.com'),
+   verification: {
+    google: '<meta name="google-site-verification" content="y4lN0VyB157omt4zTMEJ0HhoR6li0sUz07kSpfBOnRY" />',
+  },
 };
 
 export default function RootLayout({
